7ae8fb258fe326ca988a0f9adb4512dbaf133853
Sophie Preckler-Quisquater <squisquater@ucdavis.edu>
2022-05-02 22:40:10 -0700

    Create fastStructure_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25_pruned.sh

---
+++ Population-Structure/fastStructure/prunedSNPset/fastStructure_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25_pruned.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#SBATCH -t 10:00:00
+#SBATCH -p high
+#SBATCH --mem=10G
+#SBATCH -o "fastStructure_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25_pruned.out"
+#SBATCH -e "fastStructure_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25_pruned.err"
+
+for i in {2..7}
+do
+   python /home/hennelly/bin/fastStructure/structure.py -K ${i} --input=/home/sophiepq/GrayFox/Stacks_Outputs/GrayFoxAll_284/poprun_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25/populations.plink  --output=/home/sophiepq/GrayFox/Stacks_Outputs/GrayFoxAll_284/poprun_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25/populations.plink_fastStructure_K${i}
+done
