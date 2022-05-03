9934c2a6b9c49c42d805be2e157aa449e8b73349
Sophie Preckler-Quisquater <squisquater@ucdavis.edu>
2022-05-02 22:42:55 -0700

    Update fastStructure_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25_pruned.sh

--- Population-Structure/fastStructure/prunedSNPset/fastStructure_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25_pruned.sh
+++ Population-Structure/fastStructure/prunedSNPset/fastStructure_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25_pruned.sh
@@ -7,5 +7,5 @@
 
 for i in {2..7}
 do
-   python /home/hennelly/bin/fastStructure/structure.py -K ${i} --input=/home/sophiepq/GrayFox/Stacks_Outputs/GrayFoxAll_284/poprun_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25/populations.plink  --output=/home/sophiepq/GrayFox/Stacks_Outputs/GrayFoxAll_284/poprun_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25/populations.plink_fastStructure_K${i}
+   python /home/hennelly/bin/fastStructure/structure.py -K ${i} --input=/home/sophiepq/GrayFox/Stacks_Outputs/GrayFoxAll_284/poprun_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25/unlinked/populations.plink_recodebinary  --output=/home/sophiepq/GrayFox/Stacks_Outputs/GrayFoxAll_284/poprun_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25/unlinked/populations.plink_fastStructure_K${i}
 done
