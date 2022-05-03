e1a77007f8320a93dd6b85ec76449226b62f93b7
Sophie Preckler-Quisquater <squisquater@ucdavis.edu>
2022-05-02 19:41:47 -0700

    Update Populations_GrayFox_259_unlinked.sh

--- PopStats/Populations_GrayFox_259_unlinked.sh
+++ PopStats/Populations_GrayFox_259_unlinked.sh
@@ -11,3 +11,16 @@ populations -P ~/GrayFox/Stacks_Outputs/GrayFoxAll_284/ -O ~/GrayFox/Stacks_Outp
 --write-random-snp \
 --fstats -k --smooth --sigma 150000 --bootstrap \
 --structure --vcf --plink --radpainter --genepop --treemix \
+
+
+#Removed 1786658 loci that did not pass sample/population constraints from 1817068 loci.
+#Kept 30410 loci, composed of 2691200 sites; 0 of those sites were filtered, 30410 variant sites remained.
+#    1911442 genomic sites, of which 749905 were covered by multiple loci (39.2%).
+#Mean genotyped sites per locus: 88.47bp (stderr 0.07).
+
+#Population summary statistics (more detail in populations.sumstats_summary.tsv):
+#  East: 89.905 samples per locus; pi: 0.10743; all/variant/polymorphic sites: 2690468/30410/18472; private alleles: 934
+#  West: 155.26 samples per locus; pi: 0.19518; all/variant/polymorphic sites: 2690468/30410/29476; private alleles: 11938
+
+#Population pair divergence statistics (more in populations.fst_summary.tsv and populations.phistats_summary.tsv):
+#  East-West: mean Fst: 0.12395; mean Phi_st: 0.19296; mean Fst': 0.12552; mean Dxy: 0.0036008
