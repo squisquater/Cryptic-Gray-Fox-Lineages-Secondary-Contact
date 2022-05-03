a7f7147169933f321bd6eca0d30679360c6a1047
Sophie Preckler-Quisquater <squisquater@ucdavis.edu>
2022-05-02 22:16:08 -0700

    Create plink_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25_recodebinary.sh

---
+++ Population-Structure/fastStructure/fullSNPset/plink_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25_recodebinary.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -l
+#SBATCH -t 10:00
+#SBATCH -p high
+#SBATCH --mem=200M
+#SBATCH -o "plink_recode.out"
+
+module load plink
+
+plink --file /home/sophiepq/GrayFox/Stacks_Outputs/GrayFoxAll_284/poprun_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25/populations.plink --make-bed --out ~/home/sophiepq/GrayFox/Stacks_Outputs/GrayFoxAll_284/poprun_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25/populations.plink_recodebinary
