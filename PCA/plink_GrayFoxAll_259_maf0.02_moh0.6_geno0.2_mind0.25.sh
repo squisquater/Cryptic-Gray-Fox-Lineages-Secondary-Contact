06a57752306f9e0535b0f5d475a1683642a662c9
Sophie Preckler-Quisquater <squisquater@ucdavis.edu>
2022-05-04 19:16:07 -0700

    Update plink_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25.sh

--- PCA/plink_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25.sh
+++ PCA/plink_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25.sh
@@ -6,26 +6,6 @@
 
 module load plink
 
-plink --file /home/sophiepq/GrayFox/Stacks_Outputs/GrayFoxAll_284/poprun_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25/populations.plink --allow-extra-chr --dog --pca --mds-plot 2 --cluster --recode --out /home/sophiepq/GrayFox/Stacks_Outputs/GrayFoxAll_284/poprun_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25/clustering
+plink --file /home/sophiepq/GrayFox/Stacks_Outputs/GrayFoxAll_284/poprun_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25/populations.plink --allow-extra-chr --dog --pca 258 --mds-plot 2 --cluster --recode --out /home/sophiepq/GrayFox/Stacks_Outputs/GrayFoxAll_284/poprun_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25/clustering
 
-#This resulted in the following eigenvalues (default 20)
-#61.8734
-#8.32637
-#3.55333
-#2.5566
-#2.41043
-#2.21752
-#2.09845
-#2.04105
-#1.9766
-#1.84235
-#1.7639
-#1.73282
-#1.68545
-#1.68011
-#1.58843
-#1.52006
-#1.4813
-#1.44151
-#1.40426
-#1.4002
+## Run with (n - 1) PCs, where n = number of individuals
