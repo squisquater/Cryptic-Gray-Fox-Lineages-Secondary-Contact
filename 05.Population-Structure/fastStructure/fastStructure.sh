8e7b72b96389067d0c3e8ecf49e557900eb27459
Sophie Preckler-Quisquater <squisquater@ucdavis.edu>
2022-05-18 14:39:47 -0700

    Create fastStructure.sh

---
+++ 05.Population-Structure/fastStructure/fastStructure.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#SBATCH -t 10:00:00
+#SBATCH -p high
+#SBATCH --mem=10G
+#SBATCH -o "fastStructure_K2_7.out"
+#SBATCH -e "fastStructure_K2_7.err"
+
+for i in {2..7}
+do
+   python structure.py -K ${i} --input=populations.plink_recodebinary  --output=populations.plink_fastStructure
+done
