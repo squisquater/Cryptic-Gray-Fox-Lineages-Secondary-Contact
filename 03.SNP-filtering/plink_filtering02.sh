25129f3367e787f3dc53674044a638a18e682458
Sophie Preckler-Quisquater <squisquater@ucdavis.edu>
2022-05-18 15:01:29 -0700

    Update plink_filtering02.sh

--- 03.SNP-filtering/plink_filtering02.sh
+++ 03.SNP-filtering/plink_filtering02.sh
@@ -6,5 +6,5 @@
 
 module load plink
 
-plink --file plinkfileprefix --mind 0.32 --allow-extra-chr --dog --pca 259 --cluster --recode --out plink_filtered_output
+plink --file plinkfileprefix --mind 0.32 --allow-extra-chr --dog --recode --out plink_filtered02_output
 
