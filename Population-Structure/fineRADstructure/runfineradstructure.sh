6a105825c4e0de0adbc20e8e79c04614186e7bd9
Sophie Preckler-Quisquater <squisquater@ucdavis.edu>
2022-05-18 15:51:16 -0700

    Delete runfineradstructure.sh

--- Population-Structure/fineRADstructure/runfineradstructure.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-#SBATCH -t 10:00:00
-#SBATCH -p med
-#SBATCH --mem=8G
-#SBATCH -o "runfineradstructure.out"
-
-~/bin/fineRADstructure/finestructure -x 100000 -y 100000 -z 1000 populations.haps_chunks.out fineRADstruoutput.mcmc.xml
-
-#include appropriate filepaths to fineRADstructure folder, _chunks.out file, and .mcmc.xml file
