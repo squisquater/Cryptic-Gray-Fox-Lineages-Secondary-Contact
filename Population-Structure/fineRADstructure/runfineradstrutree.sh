2097ec251ec093cc8e4aefdd038df7d4e46cc943
Sophie Preckler-Quisquater <squisquater@ucdavis.edu>
2022-05-18 15:51:20 -0700

    Delete runfineradstrutree.sh

--- Population-Structure/fineRADstructure/runfineradstrutree.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-#SBATCH -t 10:00:00
-#SBATCH -p med
-#SBATCH --mem=8G
-#SBATCH -o "runfineradstrutree.out"
-
-~/bin/fineRADstructure/finestructure -m T -x 10000 populations.haps_chunks.out fineRADstruoutput.mcmc.xml fineRADstruoutput.tree.xml
-
-#include appropriate filepaths to fineRADstructure folder, _chunks.out file, .mcmc.xml file, and .tree.xml file
