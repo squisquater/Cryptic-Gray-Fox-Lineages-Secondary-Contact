83f1a1eb856e4353f6ee3c26ebac03de4a2b933b
Sophie Preckler-Quisquater <squisquater@ucdavis.edu>
2022-05-18 15:47:00 -0700

    Delete plink_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25_pruned_recodebinary.sh

--- Population-Structure/fastStructure/prunedSNPset/plink_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25_pruned_recodebinary.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-#SBATCH -t 10:00
-#SBATCH -p high
-#SBATCH --mem=200M
-#SBATCH -o "plink_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25_pruned_recodebinary.out"
-
-module load plink
-
-plink --file /home/sophiepq/GrayFox/Stacks_Outputs/GrayFoxAll_284/poprun_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25/unlinked/populations.plink --allow-extra-chr --dog --make-bed --out /home/sophiepq/GrayFox/Stacks_Outputs/GrayFoxAll_284/poprun_GrayFoxAll_259_maf0.02_moh0.6_geno0.2_mind0.25/unlinked/populations.plink_recodebinary
