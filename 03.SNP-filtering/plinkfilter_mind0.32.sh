6dd20d858be5ccb6e271e75a5b73571451025534
Sophie Preckler-Quisquater <squisquater@ucdavis.edu>
2022-05-20 14:17:09 -0700

    Update plinkfilter_mind0.32.sh

--- 03.SNP-filtering/plinkfilter_mind0.32.sh
+++ 03.SNP-filtering/plinkfilter_mind0.32.sh
@@ -6,5 +6,5 @@
 
 module load plink
 
-plink --file plinkfilter_mind0.9_geno0.2 --mind 0.32 --allow-extra-chr --dog --recode --out plinkfilter_mind0.9_geno0.2_mind0.32
+plink --file $INPUT --mind 0.32 --allow-extra-chr --dog --recode --out $OUTPUT
 
